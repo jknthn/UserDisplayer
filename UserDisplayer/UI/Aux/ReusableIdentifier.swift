549f40cb1120885a12accad80a967b2ad9cc76c9
Jeremi Kaczmarczyk <jeremi.kaczmarczyk@gmail.com>
2017-02-03 19:29:57 +0100

    Added ReusableIdentifier utilities

---
+++ UserDisplayer/UI/Aux/ReusableIdentifier.swift
@@ -0,0 +1,9 @@
+//
+//  ReusableIdentifier.swift
+//  UserDisplayer
+//
+//  Created by Jeremi Kaczmarczyk on 03/02/2017.
+//  Copyright © 2017 Jeremi Kaczmarczyk. All rights reserved.
+//
+
+import Foundation
