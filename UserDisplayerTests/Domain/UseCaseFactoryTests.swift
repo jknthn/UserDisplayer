bc5e2e90816ae2fb34aff2de06229479a38aaa19
Jeremi Kaczmarczyk <jeremi.kaczmarczyk@gmail.com>
2017-02-03 20:10:58 +0100

    Added UseCaseFactory tests

---
+++ UserDisplayerTests/Domain/UseCaseFactoryTests.swift
@@ -0,0 +1,55 @@
+//
+//  UseCaseFactoryTests.swift
+//  UserDisplayer
+//
+//  Created by Jeremi Kaczmarczyk on 03/02/2017.
+//  Copyright © 2017 Jeremi Kaczmarczyk. All rights reserved.
+//
+
+import XCTest
+@testable import UserDisplayer
+
+class UseCaseFactoryTests: XCTestCase {
+    
+    var useCaseFactory: UseCaseFactory!
+    var entityGateway: EntityGateway!
+    var closureWasInvoked = false
+    
+    // MARK: - XCTestCase
+    
+    override func setUp() {
+        entityGateway = EntityGatewayMock()
+        useCaseFactory = UseCaseFactory(entityGateway: entityGateway)
+        closureWasInvoked = false
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        useCaseFactory = nil
+        entityGateway = nil
+        closureWasInvoked = false
+        super.tearDown()
+    }
+    
+    // MARK: - Tests
+    
+    func testFactoryCreatesShowUserListUseCase() {
+        let useCase = useCaseFactory.create(useCase: .showUserList(completion: { _ in }))
+        XCTAssertNotNil(useCase as? ShowUserListUseCase)
+    }
+    
+    func testShowUserListClosureInvoked() {
+        let useCase = useCaseFactory.create(useCase: .showUserList(completion: { _ in self.closureWasInvoked = true }))
+        useCase.execute()
+        XCTAssertTrue(closureWasInvoked)
+    }
+    
+    // MARK: - Mocks
+    
+    class EntityGatewayMock: EntityGateway {
+        
+        func getUsers(completion: @escaping ([User]) -> Void) {
+            completion([])
+        }
+    }
+}
