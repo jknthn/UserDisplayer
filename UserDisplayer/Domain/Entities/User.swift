27a0a2f3b57acdb9c19d9e7538d84f763396b866
Jeremi Kaczmarczyk <jeremi.kaczmarczyk@gmail.com>
2017-02-03 18:03:12 +0100

    Added User entity

---
+++ UserDisplayer/Domain/Entities/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  UserDisplayer
+//
+//  Created by Jeremi Kaczmarczyk on 03/02/2017.
+//  Copyright © 2017 Jeremi Kaczmarczyk. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    
+    let id: Int
+    let name: String
+    let userName: String
+    let email: String
+}
